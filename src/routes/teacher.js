27de7e129eea997733e512a10d9bfe51933c8205
hai eliasi <eliasihai@hotmail.com>
2019-12-24 00:42:48 +0200

    subject get

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -114,7 +114,7 @@ router.get('/teacher/listview', function(req, res) {
     })
 });
 
-router.post('/teacher/subject', function(req, res) {
+router.get('/teacher/subject', function(req, res) {
     var subject = req.body.subject;
 
     TeacherModel.find({ subject: subject }, function(err, teacher) {
