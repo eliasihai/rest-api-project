2bb054d6149f891a3f36cde40045b0ad163b076b
hai eliasi <eliasihai@hotmail.com>
2019-12-26 18:43:25 +0200

    subj fie

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -116,24 +116,16 @@ router.post('/teacher/register1', async(req, res) => {
 router.get('/teacher/:subject', function(req, res) {
     var subject = req.body.subject;
 
-    TeacherModel.findOne({ subject: subject }, function(err, teacher) {
-        if (err) {
-            console.log(err);
-            return res.status(500).send();
-        }
-        if (!teacher) {
-            res.json({
-                status: 'error - teacher not found'
-            })
-            return res.status(404).send();
-        } else {
-            res.json({
-                status: 'ok',
-                data: teacher
-            })
-            res.status(200).send();
-        }
-    })
+    try {
+        const teacher = await TeacherModel.find(subject: subject);
+
+        res.json({
+            status: 'ok',
+            data: teacher
+        })
+    } catch (err) {
+        res.json({ message: err })
+    }
 });
 
 
