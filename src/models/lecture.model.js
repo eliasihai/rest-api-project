13674accd18e89ace952104dd8d4e79943dfbb6f
hai eliasi <eliasihai@hotmail.com>
2019-12-29 18:44:01 +0200

    lecture

--- src/models/lecture.model.js
+++ src/models/lecture.model.js
@@ -2,24 +2,28 @@ let mongoose = require('mongoose');
 require('dotenv/config');
 
 
-// mongoose.connect(
-//     process.env.DB_CONNECTION,
-//     {useNewUrlParser: true},
-//     () => console.log('connected to DB!')
-// );
-
 let LectureSchema = new mongoose.Schema({
-    date: {
+    /*date: {
         type:Date,
         unique:false
+    },*/
+    start: {
+        type: String,
+        unique: false
     },
-    hour: {
-        type:String,
-        unique:false
+    end: {
+        type: String,
+        unique: false
+    },
+    title: {
+        type: String,
+        unique: false
+    },
+    fullName: {
+        type: String,
+        unique: false
     }
-    //student_code:{},
-    //teacher_code:{},
-    //subject_code:{}
+
 })
 
 const Lecture = module.exports = mongoose.model('Lecture', LectureSchema)
